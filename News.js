31f5ef0df16bc995adef78bc7f43f13fe6fd9629
jazeymich <josephkabera42@gmail.com>
2024-04-18 16:31:31 +0300

    final:search input and field

--- News.js
+++ News.js
@@ -1,13 +1,48 @@
-const apikey = "5e29b6b5347964ae8a43241755bdb6934";
+const apikey = "e29b6b5347964ae8a43241755bdb6934";
 
 const blogContainer = document.getElementById("blog-container");
 
 /*
  if our function is unable to fetch the data it will throw an error
  */
+
+const searchField = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", async () => {
+  const query = searchField.value.trim();
+
+  if (query !== "") {
+    try {
+      const articles = await fetchNewsQuery(query);
+
+      displayBlogs(articles);
+    } catch (error) {
+      console.log("Error fetching news by query", error);
+    }
+  }
+});
+
+async function fetchNewsQuery(query) {
+  try {
+    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apikey=${apikey}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    return data.articles;
+  } catch (error) {
+    console.error("error fetching random news", error);
+    return [];
+    /*
+  it will return an empty array if it is unable to fetch the data
+  */
+  }
+}
+
 async function fetchRandomNews() {
   try {
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}`;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=20&apikey=${apikey}`;
 
     const response = await fetch(apiUrl);
     const data = await response.json();
@@ -34,14 +69,31 @@ function displayBlogs(articles) {
     img.alt = article.title;
 
     const title = document.createElement("h2");
-    title.textContent = article.title;
+
+    const truncatedTitle =
+      article.title.length > 30
+        ? article.title.slice(0, 30) + "...."
+        : article.title;
+
+    title.textContent = truncatedTitle;
 
     const description = document.createElement("p");
     description.textContent = article.description;
 
+    const truncatedDescription =
+      article.description.length > 120
+        ? article.description.slice(0, 120) + "...."
+        : article.description;
+
+    description.textContent = truncatedDescription;
+
     blogCard.appendChild(img);
     blogCard.appendChild(title);
     blogCard.appendChild(description);
+    blogCard.addEventListener("click", () => {
+      window.open(article.url, "_blank");
+    });
+
     blogContainer.appendChild(blogCard);
   });
 }
